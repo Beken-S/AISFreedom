91fc8b44e125423e03bf4418bc00b372162eb6c3
Beken Sarsenbaev <sbeken6@gmail.com>
2022-08-11 01:09:42 +0300

    fix: удалени файла
    Удалил server.js, который забыл добавить в предыдущий коммит.

--- server/server.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-
-const { PORT } = require('./config/config.js');
-
-const app = express();
-
-app.listen(PORT, () => {
-  console.log(`Server started on port ${PORT}.`);
-});
