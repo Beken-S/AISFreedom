3308304f0e36679250ac930bb73b781538d149ee
Beken Sarsenbaev <sbeken6@gmail.com>
2022-08-18 11:15:07 +0300

    fix: настройки eslint
    Исправил ошибку import/no-unresolved для алиасов.

--- client/config-overrides.js
+++ client/config-overrides.js
@@ -1,24 +1,24 @@
 const { alias } = require('react-app-rewire-alias');
 
 module.exports = function override(config, env) {
-    alias({
-        '@components': 'src/components',
-        '@ui': 'src/components/UI',
-        '@containers': 'src/containers',
-        '@constants': 'src/constants',
-        '@services': 'src/services',
-        '@utils': 'src/utils',
-        '@static': 'src/static',
-        '@styles': 'src/styles',
-        '@routes': 'src/routes',
-        '@hooks': 'src/hooks',
-        '@store': 'src/store',
-        '@context': 'src/context',
-        '@assets': 'src/assets',
-        '@scss': 'src/scss',
-        '@pages': 'src/pages',
-        '@modules': 'src/modules',
-    })(config);
+  alias({
+    '@components': 'src/components',
+    '@ui': 'src/components/UI',
+    '@containers': 'src/containers',
+    '@constants': 'src/constants',
+    '@services': 'src/services',
+    '@utils': 'src/utils',
+    '@static': 'src/static',
+    '@styles': 'src/styles',
+    '@routes': 'src/routes',
+    '@hooks': 'src/hooks',
+    '@store': 'src/store',
+    '@context': 'src/context',
+    '@assets': 'src/assets',
+    '@scss': 'src/scss',
+    '@pages': 'src/pages',
+    '@modules': 'src/modules',
+  })(config);
 
-    return config;
-}
+  return config;
+};
