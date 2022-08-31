aeab241689df7aa6fbcb603dd922f243bacc046b
diana-kot <57452102+diana-kot@users.noreply.github.com>
2022-08-31 09:49:51 +0400

    reating

--- client/src/pages/AboutProjectPage/index.js
+++ client/src/pages/AboutProjectPage/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 
 import About from '../../components/About';
-import StarReating from '../../components/StarReating';
 
 import styles from './AboutProjectPage.module.scss';
 
@@ -9,7 +8,6 @@ const AboutProjectPage = () => {
   return (
     <>
       <h1 className={styles.About__heading}>О ПРОЕКТЕ</h1>
-      <StarReating />
       <About />
     </>
   );
