242b506051d2a7253bb24c897fd0af8e755a0854
Beken Sarsenbaev <sbeken6@gmail.com>
2022-09-07 09:07:18 +0300

    fix: удалил лишнее

--- server/src/middlewares/index.ts
+++ server/src/middlewares/index.ts
@@ -2,12 +2,5 @@ import errorHandler from './errorHandler';
 import logErrors from './logErrors';
 import logRequests from './logRequests';
 import security from './security';
-import validationErrorHandler from './validationErrorHandler';
 
-export {
-  errorHandler,
-  logErrors,
-  logRequests,
-  security,
-  validationErrorHandler,
-};
+export { errorHandler, logErrors, logRequests, security };
